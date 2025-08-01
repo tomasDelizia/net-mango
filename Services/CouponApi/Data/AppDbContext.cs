5f6dfb196d527ac289deb6f7d135167b8d6bbc12
Tomás Delizia Elorza <tomidelizia@gmail.com>
2025-08-01 13:59:20 +0200

    Seed Coupons table with initial data in AppDbContext and migration files

--- Services/CouponApi/Data/AppDbContext.cs
+++ Services/CouponApi/Data/AppDbContext.cs
@@ -6,4 +6,24 @@ namespace CouponApi.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Seed database
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 1,
+            CouponCode = "10OFF",
+            DiscountAmount = 10,
+            MinAmount = 20
+        });
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 2,
+            CouponCode = "20OFF",
+            DiscountAmount = 20,
+            MinAmount = 40
+        });
+    }
 }
